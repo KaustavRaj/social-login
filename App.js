3b53df60afc5cbe5f9415028a820bf92c119aba3
Kaustav Bhattacharjee <kaustav.bhattacharjee2000@gmail.com>
2021-08-03 13:23:13 +0530

    github workflow

--- App.js
+++ App.js
@@ -5,15 +5,6 @@ import { StyleSheet, Text, View } from "react-native";
 
 import RootNavigation from "./navigation/RootNavigation";
 
-// const App = () => {
-//   return (
-//     <View style={styles.container}>
-//       <Text>Open up App.js to start working on your app!</Text>
-//       <StatusBar style="auto" />
-//     </View>
-//   );
-// };
-
 const App = () => {
   return <RootNavigation />;
 };
